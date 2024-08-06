1072a6f3c4c8460313dc429da9c8cd4376ff2f0e
Jonas Almeida <jalmeida@mathbiol.org>
2024-08-06 16:53:48 -0400

    locaforage fixed

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 console.log(`index.js loaded at\n${Date()}`);
 
 (async function(){
-    const localforage = await import('https://esm.sh/localforage@1.10.0')
-    //if(await localForage.getItem('seroHub'))
-    const seroHub = await (await import(`https://episphere.github.io/serohub/seroHub.mjs`)).seroHub
-    seroHubMsg.innerHTML=`... data loaded ${Date().toString().slice(0,24)}`
-    
+    //const localForage = (await import('https://cdn.jsdelivr.net/npm/localforage@1.10.0/+esm')).default;
+    const localForage = (await import('https://esm.run/localforage@1.9.0/src/localforage.js')).default
+    console.log('length:',await localForage.getItem('seroHub'))
+    let seroHub
+    if( !await localForage.getItem('seroHub')){
+        seroHub = await (await import(`https://episphere.github.io/serohub/serohub.mjs`)).seroHub
+        localForage.setItem('seroHub',seroHub)
+    } else {
+        seroHub = await localForage.getItem('seroHub')
+    }
+    // const seroHub = await (await import(`https://episphere.github.io/serohub/serohub.mjs`)).seroHub
+    seroHubMsg.innerHTML=`> ${seroHub.dt.seroprevalences.length} records loaded, ${Date().toString().slice(0,24)}`
     return seroHub
 })()
