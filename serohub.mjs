6e34ea1950a986f8d539e9f9278fe8755fdc1b3b
Jonas Almeida <jalmeida@mathbiol.org>
2024-08-14 18:51:34 -0400

    css conflict with observable excellent markup

--- serohub.mjs
+++ serohub.mjs
@@ -5,15 +5,15 @@ if(typeof(define)=='undefined'){
 }else{
     define(['https://cdn.plot.ly/plotly-2.34.0.min.js'],function(mod){Plotly=mod})
 }
-
-//debugger
-//console.log(`ploting with`,Plotly)
+// debugger
+console.log(`ploting with`,Plotly)
 //const Plotly = (await import("https://episphere.github.io/plotly/esm.mjs")).esm
 const localForage = (await import('https://esm.run/localforage@1.9.0/src/localforage.js')).default
 const url = 'https://episphere.github.io/serohub/seroprevalence.json.zip'
 let seroHub={
     loadedAt:Date(),
     loadedFrom:url,
+    Plotly:Plotly
 }
 let dt = await localForage.getItem('seroHub_Prevalence')
 //if(true){
